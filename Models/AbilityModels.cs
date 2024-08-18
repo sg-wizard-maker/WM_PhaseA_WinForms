f612d3b80c6f23002efcef06cb03cad14b37a260
Sam Glasby <sam.glasby@gmail.com>
2024-08-18 00:57:48 -0600

    Tweak docs

--- Models/AbilityModels.cs
+++ Models/AbilityModels.cs
@@ -10,17 +10,18 @@ using System.Xml.Serialization;
 namespace WizardMakerPrototype.Models;
 
 /// <summary>
-/// These are groupings of related Abilities for display, such as:
-///     Martial, Physical, Social, Languages, Crafts, Professions, 
-///     Academic, Supernatural, Arcane, Area Lores, Organization Lores
+/// These are groupings of related Abilities for display, such as:<br/>
+///  - Martial, Physical, Social, Languages, Crafts, Professions, <br/>
+///  - Academic, Supernatural, Arcane, Area Lores, Organization Lores<br/>
+/// <br/>
 /// Note that the names of some of these groupings are identical to 
-/// the names of AbilityTypes, such as:
-///     (Martial, Academic, Arcane, Supernatural)
-///     
+/// the names of AbilityTypes, such as:<br/>
+/// - (Martial, Academic, Arcane, Supernatural)<br/>
+/// <br/>
 /// AbilityCategories are not defined as such in the ArM5 rules,
-/// but rather are used for convenience of display.
-/// - Some people prefer a single alphabetized list, which has advantages for a very short list of Abilities.
-/// - Others prefer a number of AbilityCategories (possibly alphabetized within each).
+/// but rather are used for convenience of display.<br/>
+/// - Some people prefer a single alphabetized list, which has advantages for a very short list of Abilities.<br/>
+/// - Others prefer a number of AbilityCategories (possibly alphabetized within each).<br/>
 /// </summary>
 public static class AbilityCategory
 {
